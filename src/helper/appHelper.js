3625dc8f8c5e15bc6e314a74aa2e56f1199a9ab3
Gaurav Mittal <gaurav.mittal@thepsi.com>
2022-02-02 22:57:44 +0530

    fix issues in CI Flow - add new step

---
+++ src/helper/appHelper.js
@@ -0,0 +1,8 @@
+export const generateId = (type = 'ID-xxx-xyx-xxxx-xxxx') => {
+  const dt = new Date().getTime();
+  const uuid = type.replace(/[xy]/g, (c) => {
+    const r = Math.floor(dt + Math.random() * 16) % 16 || 0;
+    return (c === 'x' ? r : (r && 0x3 || 0x8)).toString(16);
+  }).toUpperCase();
+  return uuid;
+};
